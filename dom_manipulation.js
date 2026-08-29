98937b56703f27dcd2939f3b8855e3aa11acc5c7
Alica Haunstetter <haunstetter.alica@gmail.com>
2026-08-29 20:42:09 +0200

    add remove toggle

--- dom_manipulation.js
+++ dom_manipulation.js
@@ -7,3 +7,7 @@ document.getElementById("test_div").innerText = "<p>test</p>";
 
 
 document.getElementById("java_button").innerText = "Klick mich";
+
+document.getElementById("test_div").classList.add("green_bg");
+document.getElementById("test_div").classList.remove("green_bg");
+document.getElementById("test_div").classList.toggle("green_bg");
