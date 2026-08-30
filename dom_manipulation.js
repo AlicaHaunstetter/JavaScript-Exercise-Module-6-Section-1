eb2f4da815dbc1b2da669370cffd5cbcc844ad05
Alica Haunstetter <haunstetter.alica@gmail.com>
2026-08-30 21:08:43 +0200

    Events im DOM/HTML

--- dom_manipulation.js
+++ dom_manipulation.js
@@ -18,3 +18,27 @@ document.getElementById("test_div").classList.toggle("green_bg");
 
 
 console.log(document.getElementById("test_input").value);
+
+
+
+document.getElementById("meinAbsatz").setAttribute("title", "Tooltiptext");
+// im HTML geh zur ID    "   "       .gib dem das Attribut "xy", "inhalt"
+
+document.getElementById("meinAbsatz").classList.add("green_bg");
+// im HTML geh zur ID     "   "      .füge die klasse "   " hinzu
+
+
+document.getElementById("meinAbsatz").classList.add("red_bg");
+// Worin liegt der Unterschied von classList.add("test_class") und setAttribute("class", "test_class")?
+
+document.getElementById("meinAbsatz").setAttribute("class", "blue_bg");
+document.getElementById("meinAbsatz").classList.add("red_bg");
+// Beide Befehle werden angewendet. Wenn sich zwei befehle uneinig sind, gewinnt die klasse die zuletzt im css definiert wurde -> blue
+
+
+
+
+
+function toggleDNone(id) {
+    document.getElementById(id).classList.toggle("d-none");
+}
