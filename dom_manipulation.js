c8fffa1aa44b8004d1aebda1cec6dd362a0fd428
Alica Haunstetter <haunstetter.alica@gmail.com>
2026-08-30 20:02:36 +0200

    setAttribute

--- dom_manipulation.js
+++ dom_manipulation.js
@@ -11,3 +11,10 @@ document.getElementById("java_button").innerText = "Klick mich";
 document.getElementById("test_div").classList.add("green_bg");
 document.getElementById("test_div").classList.remove("green_bg");
 document.getElementById("test_div").classList.toggle("green_bg");
+
+
+// document.getElementById("test_input").setAttribute("type", "text");
+// document.getElementById("test_input").setAttribute("value", 123);
+
+
+console.log(document.getElementById("test_input").value);
